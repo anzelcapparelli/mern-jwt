9ce105c96065a7e80d5794920ef275abdce3c448
John Desrosiers <john.desro@gmail.com>
2021-02-23 14:04:52 -0800

    client: remove axios interceptor from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+
 import "./index.css";
 import App from "./App";
-import AuthService from "./util/AuthService";
+
 import reportWebVitals from "./reportWebVitals";
-
-const auth = new AuthService();
-
-// add an interceptor (kind of like middleware) that will add auth headers to
-// requests
-axios.interceptors.request.use(function (config) {
-  // add authorization headers for requests to own api (request url starts with
-  // "/api" exactly)
-  const token = auth.getToken();
-  if (token && /^\/api/.test(config.url)) {
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
-});
 
 ReactDOM.render(
   <React.StrictMode>
